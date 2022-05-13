0693e3aa3cfddf450121dfe011381b1e9ed7599a
youssef Shaban <85808789+youssef-shaban@users.noreply.github.com>
2022-05-14 01:04:37 +0200

    Adding nav bar

--- main.py
+++ main.py
@@ -1,20 +1,39 @@
 import streamlit as st
 from PIL import Image
-import cv2
 import numpy as np
 import model
-st.write("""
-        # X-ray Image Recognition
-        ### our website can used to check if you have COVID-19 just from checking you X-ray scan on chest.
-        so please don't be shy and upload your Scan
-""")
-uploaded_image = st.file_uploader("Upload you X-ray Scan", type= ['png','jpg','jpeg'])
-if uploaded_image is not None:
-    img= Image.open(uploaded_image)
-    
-    st.image([img,img],width=250)
-    prediction= model.predict(np.array(img))
-    if(prediction==1):
-        st.write("COVID")
-    else:
-        st.write("NO COVID")
+from streamlit_option_menu import option_menu
+
+selected2 = option_menu(None, ["Home", "Behind Code", "About Us"], 
+icons=['house', 'bi-body-text', "bi-bookmark"], 
+menu_icon="cast", default_index=0, orientation="horizontal")
+
+
+if selected2=="Home":
+    st.write("""
+            # X-ray Image Recognition
+            ### our website can used to check if you have COVID-19 just from checking you X-ray scan on chest.
+            so please don't be shy and upload your Scan
+    """)
+    uploaded_image = st.file_uploader("Upload you X-ray Scan", type= ['png','jpg','jpeg'])
+    if uploaded_image is not None:
+        img= Image.open(uploaded_image)
+        
+        st.image([img,model.hog_img(np.array(img))],width=250,clamp=True)
+        prediction= model.predict(np.array(img))
+        if(prediction==1):
+            st.write("COVID")
+        else:
+            st.write("NO COVID")
+
+elif selected2=="Behind Code":
+    st.write("""
+        # Code area
+        ## this place is under construction
+    """)
+
+elif selected2=="About Us":
+    st.write("""
+    # About Us
+    ## this place is under construction
+    """)
--- model.py
+++ model.py
@@ -11,3 +11,12 @@ def predict(img):
         cells_per_block=(2, 2), transform_sqrt=True, block_norm='L2-Hys')
     pred= clf.predict(hog_desc.reshape(1,-1))
     return pred
+
+def hog_img(img):
+    img = cv2.resize(img, (244, 244))
+    img=cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, hog_image = feature.hog(img, orientations=9, pixels_per_cell=(8, 8),
+        cells_per_block=(2, 2), transform_sqrt=True, block_norm='L2-Hys', visualize=True)
+    hog_image = hog_image.astype('float64')
+    return hog_image
+    
